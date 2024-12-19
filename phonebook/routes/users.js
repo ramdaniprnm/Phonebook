85fe77f45abb84bbb7220aca2a1d0836b41ed7fb
Ramdani <ramdanipurnama539@gmail.com>
2024-12-19 16:42:09 +0700

    Implement pagination and search functionality for user listing; add avatar update endpoint

--- phonebook/routes/users.js
+++ phonebook/routes/users.js
@@ -4,8 +4,18 @@ const { Phonebook } = require('../models');
 
 /* GET user listing. */
 router.get('/', async function (req, res, next) {
+  const { page = '1', limit = '10', keyword = '', sort = 'asc' } = req.query;
+
   try {
-    const users = await Phonebook.findAll();
+    const users = await Phonebook.findAll({
+      limit: Number(limit),
+      offset: Number((page - 1) * limit),
+      where: {
+        name: {
+          [sort === 'asc' ? '$like' : '$not like']: `%${keyword}%`
+        }
+      }
+    });
     res.json(users)
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -23,6 +33,24 @@ router.post('/', async function (req, res, next) {
 });
 
 router.put('/:id', async function (req, res, next) {
+  try {
+    const users = await Phonebook.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id
+        },
+        returning: true,
+        plain: true
+      },
+    );
+    res.status(201).json(users)
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.put('/avatar/:id', async function (req, res, next) {
   try {
     const users = await Phonebook.update(
       req.body,
